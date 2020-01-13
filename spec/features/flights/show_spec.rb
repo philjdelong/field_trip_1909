725ed944e555baee23f621887b0bf97f038386d1
Phil DeLong <philjdelong@gmail.com>
2020-01-13 09:32:03 -0700

    Create passengers for test and pass user story 1

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe "As a visitor" do
       @flight_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
       @flight_2 = @southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
 
+      @phil = Passenger.create(
+        name: 'Phil',
+        age:  29
+      )
+
+      @courtney = Passenger.create(
+        name: 'Courtney',
+        age:  28
+      )
+
+      @flight_1.passengers << [@phil, @courtney]
+      
       visit "/flights/#{@flight_1.id}"
     end
 
@@ -19,6 +31,15 @@ RSpec.describe "As a visitor" do
       expect(page).to have_content('1300')
       expect(page).to have_content('Minneapolis')
       expect(page).to have_content('Nashville')
+    end
+
+    it "i see the airline name" do
+      expect(page).to have_content('Southwest')
+    end
+
+    it "i see all passenger names" do
+      expect(page).to have_content('Phil')
+      expect(page).to have_content('Courtney')
     end
   end
 end
