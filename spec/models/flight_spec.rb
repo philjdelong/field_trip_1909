18138fcbece06fddfca1975ab2d48519f6d7aa7d
Phil DeLong <philjdelong@gmail.com>
2020-01-13 11:19:34 -0700

    Write model test for flights adult/minor_count. Pass tests for user story 4

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -11,4 +11,41 @@ RSpec.describe Flight, type: :model do
   describe 'relationships' do
     it {should belong_to :airline}
   end
+
+  describe 'methods' do
+    before :each do
+      @southwest = Airline.create(name: "Southwest")
+
+      @flight_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @flight_2 = @southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
+
+      @jeff = Passenger.create(
+        name: 'Jeff',
+        age:  31
+      )
+
+      @phil = Passenger.create(
+        name: 'Phil',
+        age:  29
+      )
+
+      @courtney = Passenger.create(
+        name: 'Courtney',
+        age:  28
+      )
+
+      @capo = Passenger.create(
+        name: 'Capo',
+        age:  7
+      )
+
+      @flight_1.passengers << [@phil, @courtney, @capo]
+
+    end
+
+    it "count adults and minors on flight" do
+      expect(@flight_1.adult_passengers).to eq(2)
+      expect(@flight_1.minor_passengers).to eq(1)
+    end
+  end
 end
