a2d524c6f3c6d1ba5a34f4f527333de814417205
Phil DeLong <philjdelong@gmail.com>
2020-01-13 09:55:26 -0700

    Write feature test for story 3

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe "As a visitor" do
       @flight_3 = @southwest.flights.create(number: "SW3", date: "12/09/19", time: "0800", departure_city: "Columbus", arrival_city: "Louisville")
       @flight_4 = @southwest.flights.create(number: "SW4", date: "12/10/19", time: "0700", departure_city: "Baltimore", arrival_city: "Portland")
 
+      @flight_5 = @southwest.flights.create(number: "SW5", date: "12/11/19", time: "0600", departure_city: "Tampa", arrival_city: "Denver")
+
       @phil = Passenger.create(
         name: 'Phil',
         age:  29
@@ -44,5 +46,26 @@ RSpec.describe "As a visitor" do
       click_on 'SW4'
       expect(current_path).to eq("/flights/#{@flight_4.id}")
     end
+
+    it "i see a new flight form" do
+      expect(page).to have_content('Flight Number:')
+
+      fill_in 'Flight Number', with: "SW5"
+      click 'Submit'
+
+      expec(current_path).to eq("/passengers/#{@phil.id}")
+
+      expect(page).to have_content('SW5')
+    end
   end
 end
+
+# User Story 3, Assign a Passenger to a Flight
+#
+# As a visitor
+# When I visit a passengers show page
+# I see a form to add a flight
+# When I fill in the form with a flight number (assuming these will always be unique)
+# And click submit
+# I'm taken back to the passengers show page
+# And I can see the flight number of the flight I just added
